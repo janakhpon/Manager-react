25daf27e7627a8b60936d5d8c126562161a7722a
janakhpon <jnovaxer@gmail.com>
2019-12-27 20:52:54 +0630

    GDEV026

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -1,56 +1,43 @@
-import React from 'react'
-import { makeStyles } from '@material-ui/core/styles'
-import Card from '@material-ui/core/Card'
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
-import CardContent from '@material-ui/core/CardContent'
-import Typography from '@material-ui/core/Typography'
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
 
-
-const useStyles = makeStyles(theme => ({
-    card: {
-        background: "#34495e",
-        maxWidth: 800,
-        border: 0,
-        textAlign: "center",
-        overflow: "hidden",
-    },
-    bigAvatar: {
-        width: 200,
-        height: 200,
-    },
-    image: {
-        display: 'flex',
-        alignItems: "center",
-        justifyContent: "center",
-        '& > *': {
-            margin: theme.spacing(1),
-        },
-    },
-}));
+const useStyles = makeStyles({
+  card: {
+    maxWidth: '100%',
+    perspective: '1000px',
+    color: "#ffffff",
+    backgroundColor: "#1d3557",
+  },
+  media: {
+    height: '65vh',
+  },
+});
 
 export default function PageLanding() {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    return (
-        <Card className={classes.card}>
-            <CardActionArea>
-                <CardContent>
-                    <div className={classes.image}>
-                    <Typography variant="h3" gutterBottom>
-                   Manager
-                  </Typography>
-                    </div>
-
-                    <Typography
-                        className={"MuiTypography--heading"}
-                        variant={"h6"}
-                        gutterBottom
-                    >
-                        Your presonal coach for time management
+  return (
+    <Card className={classes.card}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image="Time.jpg"
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h4" component="h1" align="center">
+            MANAGE YOUR TIME EFFECTIVELY!
           </Typography>
-                    
-                </CardContent>
-            </CardActionArea>
-        </Card>
-    );
+          <Typography variant="body2" color="textSecondary" component="p" align="center">
+            Time is the most important thing in life, some lift their lives to the heighest just by manage their time EFFECTIVELY and some fall down to the bottom cuz the failed to manage it!
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
 }
